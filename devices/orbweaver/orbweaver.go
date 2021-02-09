2f195e6f15f002455988a58f075a49da42ea57c5
Minizbot2012 <bradleyfwin@gmail.com>
2021-02-09 13:41:12 -0800

    Utilty functions

--- devices/orbweaver/orbweaver.go
+++ devices/orbweaver/orbweaver.go
@@ -1,5 +1,12 @@
 package orbweaver
 
+import (
+	"encoding/binary"
+	"os"
+
+	"fyne.io/fyne"
+)
+
 //BINDING Map of Default Keys
 var BINDING = [...]byte{41, 2, 3, 4, 5, 15, 16, 17, 18, 19, 58, 30, 31, 32, 33, 42, 44, 45, 46, 47, 56, 103, 106, 108, 105, 57}
 
@@ -14,4 +21,33 @@ type KeyMaps struct {
 	Maps       [7]*KeyMap
 	Currentmap int
 	MCount     int
+}
+
+//PKM format for altering the keymap
+type PKM struct {
+	MIP [20]uint16
+	SIP [6]uint16
+	COL [3]byte
+}
+
+//LoadKM Load Orbmap KM structure
+func LoadKM(file string) *KeyMap {
+	mapped := new(KeyMap)
+	of, _ := os.Open(file)
+	binary.Read(of, binary.LittleEndian, mapped)
+	return mapped
+}
+
+//SavePKMKeymap saves an orb after edit
+func SavePKMKeymap(mapped *PKM, file fyne.URIWriteCloser) {
+	binary.Write(file, binary.LittleEndian, mapped)
+	file.Close()
+}
+
+//LoadPKMKeymap loads an orb for editing
+func LoadPKMKeymap(file fyne.URIReadCloser) *PKM {
+	mapped := new(PKM)
+	binary.Read(file, binary.LittleEndian, mapped)
+	file.Close()
+	return mapped
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/OrbTools/OrbCommon
 
 go 1.15
+
+require fyne.io/fyne v1.4.3
